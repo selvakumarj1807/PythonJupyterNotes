6ca1820a2d0c0095a03093bc4a910048db9acb88
selvakumarj1807 <selvakumarj1807@gmail.com>
2025-05-02 11:50:03 +0530

    Python Modules or Packages

---
+++ PythonModules/.ipynb_checkpoints/package-checkpoint.py
@@ -0,0 +1,12 @@
+companyName = 'Skill Fort'
+
+website = 'skillfort.com'
+
+print('---------------------------------------')
+
+def add():
+    a = int(input('Enter a value 01: '))
+
+    b = int(input('Enter a value 02: '))
+
+    print(a, '+', b, '=', a + b)
