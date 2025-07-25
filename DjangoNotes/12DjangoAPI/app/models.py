27e950fc42508f94f9f7d433ad6cb971d0d980f8
selvakumarj1807 <selvakumarj1807@gmail.com>
2025-07-25 11:59:04 +0530

    API - Example 01 Completed

--- DjangoNotes/12DjangoAPI/app/models.py
+++ DjangoNotes/12DjangoAPI/app/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Student(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, null=True, blank=True)
     email = models.EmailField(unique=True)
     course = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
