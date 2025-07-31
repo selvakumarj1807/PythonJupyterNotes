c88e903c0d3f75db837946c62ddcb36d90d8b583
selvakumarj1807 <selvakumarj1807@gmail.com>
2025-07-31 11:21:42 +0530

    Api Project 02 - step 02

--- DjangoNotes/13DjangoAPI/app/views.py
+++ DjangoNotes/13DjangoAPI/app/views.py
@@ -15,8 +15,12 @@ class StudentEnquiryViewSet(viewsets.ModelViewSet):
         serializer = StudentEnquirySerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response({
+                'message': 'StudentEnquiry Inserted successfully',
+                'data': serializer.data
+            }, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
     
     def retrieve(self, request, pk=None):
         """Get StudentEnquiry by ID"""
@@ -24,3 +28,32 @@ class StudentEnquiryViewSet(viewsets.ModelViewSet):
         serializer = StudentEnquirySerializer(studentEnquiry)
         return Response(serializer.data)
     
+    def list(self, request):
+        """List all StudentEnquiries with count"""
+        studentEnquiries = StudentEnquiry.objects.all()
+        serializer = StudentEnquirySerializer(studentEnquiries, many=True)
+        return Response({
+            'count': studentEnquiries.count(),
+            'data': serializer.data
+        })
+    
+    def update(self, request, pk=None):
+        """Update an entire StudentEnquiry"""
+        studentEnquiry = get_object_or_404(StudentEnquiry, pk=pk)
+        serializer = StudentEnquirySerializer(studentEnquiry, data=request.data, partial=False)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({
+                'message': 'StudentEnquiry Updated successfully',
+                'data': serializer.data
+            })
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def destroy(self, request, pk=None):
+        """Delete a StudentEnquiry"""
+        studentEnquiry = get_object_or_404(StudentEnquiry, pk=pk)
+        studentEnquiry.delete()
+        return Response({'message': 'StudentEnquiry deleted successfully'}, status=status.HTTP_204_NO_CONTENT)
+    
+    
+
